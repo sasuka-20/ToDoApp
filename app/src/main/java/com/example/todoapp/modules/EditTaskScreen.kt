7bc093b72a1d424ece90e8f8bae6bfaa6a23842a
Kaveen Dinethma <145594033+sasuka-20@users.noreply.github.com>
2024-05-15 01:39:44 +0530

    TD | Update functionality to Tasks

--- app/src/main/java/com/example/todoapp/modules/EditTaskScreen.kt
+++ app/src/main/java/com/example/todoapp/modules/EditTaskScreen.kt
@@ -1,7 +1,12 @@
 package com.example.todoapp.modules
 
+import Task
+import TaskViewModel
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import com.example.todoapp.R
 import com.example.todoapp.databinding.ActivityEditBinding
@@ -10,10 +15,55 @@ import com.example.todoapp.databinding.ActivityHomeBinding
 class EditTaskScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditBinding
+    private var taskId : Int = -1
+    private lateinit var taskViewModel: TaskViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        taskId = intent.getIntExtra("taskId",-1)
+
+        taskViewModel = TaskViewModel(application)
+
+        val task = taskViewModel.getTaskById(taskId)
+        if (task != null) {
+            // Display task details
+            val taskTitle = task.taskTitle
+            binding.taskTitle.text = Editable.Factory.getInstance().newEditable(taskTitle)
+            val taskDescription = task.taskDescription
+            binding.taskDescription.text = Editable.Factory.getInstance().newEditable(taskDescription)
+        }
+
+        binding.doneBtn.setOnClickListener {
+            saveEditedTask()
+        }
+
+    }
+
+    private fun saveEditedTask(){
+
+        val editedTitle = binding.taskTitle.text.toString()
+        val editedDescription = binding.taskDescription.text.toString()
+
+        val editedTask = Task(
+            id = taskId,
+            taskTitle = editedTitle,
+            taskDescription = editedDescription,
+        )
+
+        val updatedRows = taskViewModel.updateTask(editedTask)
+
+        if (updatedRows > 0) {
+            Toast.makeText(this, "Task updated successfully", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HomeScreen::class.java)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Failed to update task", Toast.LENGTH_SHORT).show()
+        }
     }
 }
